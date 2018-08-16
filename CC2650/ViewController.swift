204178acc175159bb6796c2b75ee314bb26174d0
leandroestrada <leandro.a.estrada@gmail.com>
2018-08-15 22:59:34 -0300

    Primeiro commit do sensor

---
+++ CC2650/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CC2650
+//
+//  Created by le on 15/08/2018.
+//  Copyright © 2018 LeandroEstrada. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CC2650Tests/CC2650Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CC2650Tests.swift
+//  CC2650Tests
+//
+//  Created by le on 15/08/2018.
+//  Copyright © 2018 LeandroEstrada. All rights reserved.
+//
+
+import XCTest
+@testable import CC2650
+
+class CC2650Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
