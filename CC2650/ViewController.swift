ae86eb5968829a98834389d4c0de3a1f2e1b2a32
leandroestrada <leandro.a.estrada@gmail.com>
2018-08-15 23:05:37 -0300

    Adding first BT functions

--- CC2650/ViewController.swift
+++ CC2650/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import CoreBluetooth
 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,7 +22,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var centralManager: CBCentralManager!
+    var myPeripheral: CBPeripheral!
+    @IBOutlet var lightText: UIView!
+    
+    @IBOutlet weak var temperatureText: UILabel!
+    
+    @IBOutlet weak var humidityText: UILabel!
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print ("scanning...")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name?.contains("SensorTag") == true {
+            print (peripheral.name ?? "no name")
+            centralManager.stopScan()
+            print (advertisementData)
+            central.connect(peripheral, options: nil)
+            myPeripheral = peripheral
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print ("connected \(peripheral.name)")
+        peripheral.discoverServices(nil)
+        peripheral.delegate = self
+    }
+    
 }
 
